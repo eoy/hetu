2bc53514adb709661ee42ea38c39b33d091852bc
Joakim Runeberg <joakim.runeberg@gmail.com>
2014-04-09 09:14:51 +0300

    Update hetu_validator.rb

--- lib/hetu/hetu_validator.rb
+++ lib/hetu/hetu_validator.rb
@@ -2,6 +2,16 @@ require 'active_model/validator'
 
 class HetuValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, :invalid) unless Hetu.valid?(value)
+    
+    # Create a new swedish personnummer from the value
+    p = Personnummer.new(value)
+    
+    if Hetu.valid?(value) == true                 # Is a Finnish person
+      return true
+    elsif Hetu.valid?(value) == false && p.valid? # Is a Swedish person
+      return true
+    else                                          # Is invalid
+      record.errors.add(attribute, :invalid)
+    end
   end
 end
